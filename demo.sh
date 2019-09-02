f74d618ee637894e52d72da08c458c70d30e3efb
Erik Veld <eveld@hashicorp.com>
2019-09-02 10:22:25 +0200

    Add demo script

---
+++ demo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+tiles=("consul" "vagrant" "packer" "terraform" "vault" "nomad")
+
+# Turn on hashicorp tile.
+echo "turn on hashicorp"
+curl -XPOST localhost:5000/led/hashicorp
+
+# Keep looping until we exit.
+while true; do
+    # Loop over the tiles.
+    for tile in ${tiles[*]}; do
+        # Turn them on for 5 seconds, then turn them off.
+        echo "turn on $tile"
+        curl -XPOST localhost:5000/led/$tile
+        sleep 5
+        echo "turn off $tile"
+        curl -XDELETE localhost:5000/led/$tile
+    done
+done
